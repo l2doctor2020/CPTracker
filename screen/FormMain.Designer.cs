b795e1a30b491776cd55dc73057aa433f7cfe0b0
l2 <l2doctor2020@gmail.com>
2019-09-04 03:18:11 +0300

    First commit

---
+++ screen/FormMain.Designer.cs
@@ -0,0 +1,254 @@
+﻿namespace screen
+{
+    partial class FormMain
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormMain));
+            this.button1 = new System.Windows.Forms.Button();
+            this.pictureBoxMain = new System.Windows.Forms.PictureBox();
+            this.buttonTracking = new System.Windows.Forms.Button();
+            this.labelAlarm = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.pictureBoxSecond = new System.Windows.Forms.PictureBox();
+            this.panelColor1 = new System.Windows.Forms.Panel();
+            this.panelColor2 = new System.Windows.Forms.Panel();
+            this.checkBoxSound = new System.Windows.Forms.CheckBox();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.toolStripStatusLabel = new System.Windows.Forms.ToolStripStatusLabel();
+            this.labelTime = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxMain)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxSecond)).BeginInit();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.Location = new System.Drawing.Point(24, 26);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(519, 74);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Take windows";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // pictureBoxMain
+            // 
+            this.pictureBoxMain.BackColor = System.Drawing.Color.White;
+            this.pictureBoxMain.Location = new System.Drawing.Point(24, 258);
+            this.pictureBoxMain.Name = "pictureBoxMain";
+            this.pictureBoxMain.Size = new System.Drawing.Size(520, 284);
+            this.pictureBoxMain.TabIndex = 2;
+            this.pictureBoxMain.TabStop = false;
+            this.pictureBoxMain.Click += new System.EventHandler(this.pictureBoxMain_Click);
+            // 
+            // buttonTracking
+            // 
+            this.buttonTracking.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.buttonTracking.Location = new System.Drawing.Point(564, 26);
+            this.buttonTracking.Name = "buttonTracking";
+            this.buttonTracking.Size = new System.Drawing.Size(520, 74);
+            this.buttonTracking.TabIndex = 5;
+            this.buttonTracking.Text = "Tracking point";
+            this.buttonTracking.UseVisualStyleBackColor = true;
+            this.buttonTracking.Click += new System.EventHandler(this.buttonTracking_Click);
+            // 
+            // labelAlarm
+            // 
+            this.labelAlarm.BackColor = System.Drawing.Color.White;
+            this.labelAlarm.Font = new System.Drawing.Font("Microsoft Sans Serif", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.labelAlarm.ForeColor = System.Drawing.Color.Red;
+            this.labelAlarm.Location = new System.Drawing.Point(565, 112);
+            this.labelAlarm.Name = "labelAlarm";
+            this.labelAlarm.Size = new System.Drawing.Size(520, 61);
+            this.labelAlarm.TabIndex = 6;
+            this.labelAlarm.Text = "     ";
+            this.labelAlarm.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.White;
+            this.pictureBox1.Location = new System.Drawing.Point(3, 3);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(159, 118);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 7;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackColor = System.Drawing.Color.White;
+            this.pictureBox2.Location = new System.Drawing.Point(215, 123);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(159, 118);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 8;
+            this.pictureBox2.TabStop = false;
+            this.pictureBox2.Click += new System.EventHandler(this.pictureBox2_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.Gray;
+            this.panel1.Controls.Add(this.pictureBox1);
+            this.panel1.Location = new System.Drawing.Point(25, 120);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(165, 124);
+            this.panel1.TabIndex = 9;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.Gray;
+            this.panel2.Location = new System.Drawing.Point(212, 120);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(165, 124);
+            this.panel2.TabIndex = 10;
+            // 
+            // pictureBoxSecond
+            // 
+            this.pictureBoxSecond.BackColor = System.Drawing.Color.White;
+            this.pictureBoxSecond.Location = new System.Drawing.Point(564, 258);
+            this.pictureBoxSecond.Name = "pictureBoxSecond";
+            this.pictureBoxSecond.Size = new System.Drawing.Size(520, 284);
+            this.pictureBoxSecond.TabIndex = 12;
+            this.pictureBoxSecond.TabStop = false;
+            // 
+            // panelColor1
+            // 
+            this.panelColor1.BackColor = System.Drawing.Color.White;
+            this.panelColor1.Location = new System.Drawing.Point(486, 190);
+            this.panelColor1.Name = "panelColor1";
+            this.panelColor1.Size = new System.Drawing.Size(57, 54);
+            this.panelColor1.TabIndex = 13;
+            // 
+            // panelColor2
+            // 
+            this.panelColor2.BackColor = System.Drawing.Color.White;
+            this.panelColor2.Location = new System.Drawing.Point(1027, 189);
+            this.panelColor2.Name = "panelColor2";
+            this.panelColor2.Size = new System.Drawing.Size(58, 55);
+            this.panelColor2.TabIndex = 14;
+            // 
+            // checkBoxSound
+            // 
+            this.checkBoxSound.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.checkBoxSound.AutoSize = true;
+            this.checkBoxSound.Checked = true;
+            this.checkBoxSound.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.checkBoxSound.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.checkBoxSound.Location = new System.Drawing.Point(774, 189);
+            this.checkBoxSound.Name = "checkBoxSound";
+            this.checkBoxSound.Size = new System.Drawing.Size(105, 24);
+            this.checkBoxSound.TabIndex = 15;
+            this.checkBoxSound.Text = "Play sound";
+            this.checkBoxSound.UseVisualStyleBackColor = true;
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 561);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(1105, 22);
+            this.statusStrip1.TabIndex = 16;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // toolStripStatusLabel
+            // 
+            this.toolStripStatusLabel.Name = "toolStripStatusLabel";
+            this.toolStripStatusLabel.Size = new System.Drawing.Size(0, 17);
+            // 
+            // labelTime
+            // 
+            this.labelTime.Location = new System.Drawing.Point(564, 177);
+            this.labelTime.Name = "labelTime";
+            this.labelTime.Size = new System.Drawing.Size(166, 23);
+            this.labelTime.TabIndex = 17;
+            // 
+            // FormMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.ClientSize = new System.Drawing.Size(1105, 583);
+            this.Controls.Add(this.labelTime);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.checkBoxSound);
+            this.Controls.Add(this.panelColor2);
+            this.Controls.Add(this.panelColor1);
+            this.Controls.Add(this.pictureBoxSecond);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.labelAlarm);
+            this.Controls.Add(this.buttonTracking);
+            this.Controls.Add(this.pictureBoxMain);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.panel2);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "FormMain";
+            this.Text = "CP Tracker";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxMain)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxSecond)).EndInit();
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox pictureBoxMain;
+        private System.Windows.Forms.Button buttonTracking;
+        private System.Windows.Forms.Label labelAlarm;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.PictureBox pictureBoxSecond;
+        private System.Windows.Forms.Panel panelColor1;
+        private System.Windows.Forms.Panel panelColor2;
+        private System.Windows.Forms.CheckBox checkBoxSound;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel;
+        private System.Windows.Forms.Label labelTime;
+    }
+}
+
---
+++ screen/FormMain.cs
@@ -0,0 +1,320 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace screen
+{
+    public partial class FormMain : Form
+    {
+
+        String ProcessName = "L2";
+
+        Bitmap pic1;
+        Bitmap pic2;
+
+        System.Media.SoundPlayer player;
+
+        double timeRefresh = 0.1;
+
+        bool TrackingIsActive = false;
+        Color TrackingColor;
+        int TrackingX;
+        int TrackingY;
+
+        int selectedWindow = 1;
+
+        DateTime lastSound;
+        DateTime? firstAlarm;
+        DateTime? lastUpdate;
+
+
+        public FormMain()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            CaptureWindows();
+        }
+
+        public void CaptureWindows()
+        {
+            if (Properties.Settings.Default.LastColor != null)
+            {
+                panelColor1.BackColor = Properties.Settings.Default.LastColor;
+            }
+            var items = Process.GetProcesses();
+            int i = 0;
+            foreach (var proc in items)
+            {
+                if (proc.ProcessName == ProcessName)
+                {
+                    i++;
+                    var rect = new User32.Rect();
+                    User32.GetWindowRect(proc.MainWindowHandle, ref rect);
+                    int width = rect.right - rect.left;
+                    int height = rect.bottom - rect.top;
+                    var bmp = new Bitmap(width, height, PixelFormat.Format32bppArgb);
+                    Graphics graphics = Graphics.FromImage(bmp);
+                    graphics.CopyFromScreen(rect.left, rect.top, 0, 0, new Size(width, height), CopyPixelOperation.SourceCopy);
+                    // bmp.Save("test_" + i.ToString() + ".png", ImageFormat.Png);
+                    pictureBoxMain.Image = bmp;
+                    switch (i)
+                    {
+                        case 1:
+                            pic1 = bmp;
+                            pictureBox1.Image = pic1;
+                            break;
+                        case 2:
+                            pic2 = bmp;
+                            pictureBox2.Image = pic2;
+                            break;
+                    }
+                }
+                showActiveWindow();
+            }
+        }
+
+        private void pictureBoxMain_Click(object sender, EventArgs e)
+        {
+            MouseEventArgs rato = e as MouseEventArgs;
+            Bitmap b = ((Bitmap)pictureBoxMain.Image);
+            //int x = rato.X * b.Width / pictureBoxMain.ClientSize.Width;
+            //int y = rato.Y * b.Height / pictureBoxMain.ClientSize.Height;
+            int x = rato.X;
+            int y = rato.Y;
+            Color c = b.GetPixel(x, y);
+            TrackingColor = c;
+            panelColor1.BackColor = c;
+            Properties.Settings.Default.LastColor = c;
+            TrackingX = x;
+            TrackingY = y;
+            Properties.Settings.Default.LastX = x;
+            Properties.Settings.Default.LastY = y;
+            Properties.Settings.Default.Save();
+            //String text = "color R:" + c.R.ToString() + " G:" + c.G.ToString() + " B:" + c.B.ToString();
+            //text += "\nX: " + x.ToString() + " Y: " + y.ToString();
+            //Console.WriteLine(text);
+        }
+
+        private async void buttonTracking_Click(object sender, EventArgs e)
+        {
+            firstAlarm = null;
+            if (player != null)
+            {
+                player.Stop();
+            }
+            TrackingIsActive = !TrackingIsActive;
+            if (TrackingIsActive)
+            {
+                toolStripStatusLabel.Text = "";
+                buttonTracking.ForeColor = Color.Red;
+                await Tracking();
+            }
+            else
+            {
+                buttonTracking.ForeColor = Color.Black;
+                labelAlarm.Text = "";
+            }
+        }
+
+        void showActiveWindow()
+        {
+            panel1.BackColor = Color.Gray;
+            panel2.BackColor = Color.Gray;
+            if (selectedWindow == 1)
+            {
+                panel1.BackColor = Color.Red;
+                pictureBoxMain.Image = pic1;
+            }
+            if (selectedWindow == 2)
+            {
+                panel2.BackColor = Color.Red;
+                pictureBoxMain.Image = pic2;
+            }
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            selectedWindow = 1;
+            showActiveWindow();
+            Properties.Settings.Default.LastWindow = 1;
+            Properties.Settings.Default.Save();
+        }
+
+        private void pictureBox2_Click(object sender, EventArgs e)
+        {
+            selectedWindow = 2;
+            showActiveWindow();
+            Properties.Settings.Default.LastWindow = 2;
+            Properties.Settings.Default.Save();
+        }
+
+        private async Task Tracking()
+        {
+            var items = Process.GetProcesses();
+            do
+            {
+                int i = 0;
+                foreach (var proc in items)
+                {
+                    if (proc.ProcessName == ProcessName)
+                    {
+                        i++;
+                        if (selectedWindow != i)
+                        {
+                            continue;
+                        }
+                        Color? c0 = null;
+                        Bitmap bmp = null;
+                        try
+                        {
+                            var rect = new User32.Rect();
+                            User32.GetWindowRect(proc.MainWindowHandle, ref rect);
+
+                            int width = rect.right - rect.left;
+                            int height = rect.bottom - rect.top;
+
+                            bmp = new Bitmap(width, height, PixelFormat.Format32bppArgb);
+                            Graphics graphics = Graphics.FromImage(bmp);
+                            graphics.CopyFromScreen(rect.left, rect.top, 0, 0, new Size(width, height), CopyPixelOperation.SourceCopy);
+                            graphics.Dispose();
+                            c0 = bmp.GetPixel(TrackingX, TrackingY);
+                        }
+                        catch (Exception ex)
+                        {
+                            Console.WriteLine(ex.Message);
+                        }
+                        if (c0 == null || bmp == null)
+                        {
+                            continue;
+                        }
+                        labelTime.Text = DateTime.Now.ToString();
+                        Color c = (Color)c0;
+                        panelColor2.BackColor = c;
+                        // String text = "color R:" + c.R.ToString() + " G:" + c.G.ToString() + " B:" + c.B.ToString() + "\n" + DateTime.Now.ToString();
+                        // Console.WriteLine(text);
+                        if (lastUpdate == null || lastUpdate < DateTime.Now.AddSeconds(-5))
+                        {
+                            if (pictureBoxSecond.Image != null) pictureBoxSecond.Image.Dispose();
+                            pictureBoxSecond.Image = (Bitmap)bmp.Clone();
+                            lastUpdate = DateTime.Now;
+                        }
+                        bmp.Dispose();
+                        if (c == TrackingColor)
+                        {
+                            labelAlarm.Text = "";
+                        }
+                        else
+                        {
+                            labelAlarm.Text = "Alarm!";
+                            if (checkBoxSound.Checked && (lastSound == null || lastSound < DateTime.Now.AddSeconds(-1)))
+                            {
+                                // System.Media.SystemSounds.Hand.Play();
+                                if (player != null)
+                                {
+                                    player.Play();
+                                }
+                                lastSound = DateTime.Now;
+                            }
+                            if (firstAlarm == null)
+                            {
+                                firstAlarm = DateTime.Now;
+                                toolStripStatusLabel.Text = "Time: " + firstAlarm.ToString();
+                            }
+                        }
+
+                    }
+                }
+                await Task.Delay((int)(timeRefresh * 1000));
+                // Thread.Sleep(1000);
+            } while (TrackingIsActive);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            if (Properties.Settings.Default.RefreshTime > 0)
+            {
+                timeRefresh = Properties.Settings.Default.RefreshTime;
+            }
+            if (Properties.Settings.Default.LastWindow > 0)
+            {
+                selectedWindow = Properties.Settings.Default.LastWindow;
+            }
+            if (Properties.Settings.Default.LastX > 0)
+            {
+                TrackingX = Properties.Settings.Default.LastX;
+            }
+            if (Properties.Settings.Default.LastY > 0)
+            {
+                TrackingY = Properties.Settings.Default.LastY;
+            }
+            if (Properties.Settings.Default.LastColor != null)
+            {
+                TrackingColor = Properties.Settings.Default.LastColor;
+            }
+            String path = "alarm_clock.wav";
+            if (File.Exists(path))
+            {
+                player = new System.Media.SoundPlayer(path);
+            }
+            CaptureWindows();
+        }
+
+        private class User32
+        {
+            [StructLayout(LayoutKind.Sequential)]
+            public struct Rect
+            {
+                public int left;
+                public int top;
+                public int right;
+                public int bottom;
+            }
+
+            [DllImport("user32.dll")]
+            public static extern IntPtr GetWindowRect(IntPtr hWnd, ref Rect rect);
+        }
+
+
+
+        //void GetProcesses()
+        //{
+        //    var items = Process.GetProcesses();
+        //    foreach (var item in items)
+        //    {
+        //        Console.WriteLine(item.ProcessName);
+        //    }
+        //}
+
+        //public void CaptureApplication(string procName)
+        //{
+        //    var proc = Process.GetProcessesByName(procName)[0];
+        //    var rect = new User32.Rect();
+        //    User32.GetWindowRect(proc.MainWindowHandle, ref rect);
+
+        //    int width = rect.right - rect.left;
+        //    int height = rect.bottom - rect.top;
+
+        //    var bmp = new Bitmap(width, height, PixelFormat.Format32bppArgb);
+        //    Graphics graphics = Graphics.FromImage(bmp);
+        //    graphics.CopyFromScreen(rect.left, rect.top, 0, 0, new Size(width, height), CopyPixelOperation.SourceCopy);
+
+        //    bmp.Save("test.png", ImageFormat.Png);
+        //}
+
+    }
+
+}
